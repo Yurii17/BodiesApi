3cb28c1b44638e4889928036ed7bcd6f61114bd3
Yurii17 <37590571+Yurii17@users.noreply.github.com>
2019-01-23 17:35:05 +0200

    release v1

--- tests/api/sessionsCest.php
+++ tests/api/sessionsCest.php
@@ -2,12 +2,10 @@
 
 use Faker\Factory as fake;
 
-
 class sessionsCest
 {
     public $route = '/sessions';
     public $userID;
-
 
     private function setRoute($params)
     {
@@ -23,46 +21,61 @@ class sessionsCest
         $I->loginAs("yurii.lobas+0badbae683b2c6ff5a14bfe90dcfef6d@gmail.com", "6bfAC<kkThESw2");
     }
 
-    public function _before(ApiTester $I)
-    {
-    }
-
-
-    //--------------Add new session----------------//
-
+    //-------------- Send Post Add New Session Valid -----------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
+     * @throws Exception
      */
-    public function sendPostAddNewSession(ApiTester $I)
+    public function sendPostAddNewSessionValid(ApiTester $I)
     {
         $data = [
-        "price" => 100,
-        "name" => "Test Session",
-        "description" => "Bla bla bla",
-        "type" => "fit",
-        "isGroup" => 1,
-        "inHouse" => 1,
-        "participantsMax" => fake::create()->randomNumber(2,true),
-        "trainingIntro" => 1,
-        "trainingMultiple" => 1
+            'name' => 'Test Session',
+            'description' => fake::create()->text(50),
+            'type' => 'fit',
+            'price' => fake::create()->randomNumber(2),
+            'atHome' => '1',
+            'isGroup' => 1,
+            'participantsMax' => fake::create()->randomNumber(2,true),
+            'isLevelBeginner' => '1',
+            'isLevelIntermediate' => '1',
+            'isLevelAdvanced' => '1',
+            'trainingIntro' => 1,
+            'trainingSingle' => '1',
+            'trainingMultiple' => 1,
+            'userId' => 1
         ];
         $I->sendPOST($this->route, $data);
+        $this->userID = $I->grabDataFromResponseByJsonPath('$.id');
         $I->seeResponseCodeIs(201);
     }
 
-    //------------------Edit session------------------//
+    public function sendPostAddNewSessionError(ApiTester $I)
+    {
+        $data = [
+            'price' => 1,
+            'name' => 'Test Session',
+            'description' => 'Test',
+            'type' => 'fit',
+        ];
+        $I->sendPOST($this->route, $data);
+        $I->seeForbiddenErrorMessage([
+            'name' => 'Forbidden',
+            'message' => 'Access denied'
+        ]);
+    }
+
+    //------------  Send Put Edit Session  ------------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendPutEditSession(ApiTester $I)
     {
-        $this->userID = 52;
         $data = [
             "price" => 100,
             "name" => "Test Session",
-            "description" => fake::create()->text(5,true),
+            "description" => fake::create()->text(10),
             "type" => "fit",
             "isGroup" => 1,
             "inHouse" => 1,
@@ -70,11 +83,11 @@ class sessionsCest
             "trainingIntro" => 1,
             "trainingMultiple" => 1
         ];
-        $I->sendPUT($this->route.'/'.$this->userID, $data);
+        $I->sendPUT($this->route.'/'.$this->userID[0], $data);
         $I->seeResponseCodeIs(200);
     }
 
-    //--------------------Listing of sessions--------------------//
+    //--------------- Send Get Listing of Sessions  ----------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
@@ -85,78 +98,66 @@ class sessionsCest
         $I->seeResponseCodeIs(200);
     }
 
-    //--------------------ShowSession-------------------------//
+    //--------------  Send Get Show Session   ---------------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendGetShowSession(ApiTester $I)
     {
-        $this->userID = 30;
-        $I->sendGet($this->route.'/'.$this->userID);
+        $I->sendGet($this->route.'/'.$this->userID[0]);
         $I->seeResponseCodeIs(200);
     }
 
-
-    //-------------------Delete Session---------------------//
-    /**
-     * @param ApiTester $I
-     * @before signInByPassword
-     */
-    public function sendDeleteSession(ApiTester $I)
-    {
-        $this->userID = 52;
-        $I->sendDELETE($this->route.'/'.$this->userID);
-        $I->seeResponseCodeIs(204);
-    }
-
-    //----------------Listing of trainings by Session ID----------------//
-    /**
-     * @param ApiTester $I
-     * @before signInByPassword
-     */
-    public function sendGetListingOfTrainingsBySessionID(ApiTester $I)
-    {
-        $this->userID = 22;
-        $I->sendGet($this->route.'/'.$this->userID.'/trainings');
-        $I->seeResponseCodeIs(200);
-    }
-
-    //-----------------Create trainings by Session ID----------------//
+    //-------------- Send Post Create Trainings by Session ID   ------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendPostCreateTrainingsBySessionID(ApiTester $I)
     {
-        $this->userID = 22;
         $data = [
-            'locationId' => 1,
-                'start' => [
+            'id' => 1,
+            'createdAt' => '2019-01-03 12:20:53',
+            'modifiedAt' => '2019-01-03 12:20:53',
+            'expireAt' => null,
+            'status' => null,
+            'userId' => 1,
+            'sessionId' => 1,
+            'participantsCurrent '=> 1,
+            'startDatetime' => '2018-12-12 12:00:00',
+            'endDatetime' => '2018-12-12 13:00:00',
+            'spaceId' => [
+                'id' => 15
+            ],
+            'participantsCurrent' => 1,
+            'start' => [
                 '2018-12-12 12:00:00',
                 '2018-12-13 12:00:00',
                 '2018-12-14 12:00:00'
-                ],
-                    'end' => [
-                        '2018-12-12 13:00:00',
-                        '2018-12-13 13:50:00',
-                        '2018-12-14 13:00:00'
-                        ]
+            ],
+            'end' => [
+                '2018-12-12 13:00:00',
+                '2018-12-13 13:50:00',
+                '2018-12-14 13:00:00'
+            ]
         ];
-        $I->sendGet($this->route.'/'.$this->userID.'/trainings', $data);
+        $I->sendPOST($this->route.'/'.$this->userID[0].'/trainings', $data);
         $I->seeResponseCodeIs(200);
     }
 
-    //------------Update trainings by Session ID---------------//
+    //------------  Send Put Update Trainings by Session ID   -------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendPutUpdateTrainingsBySessionId(ApiTester $I)
     {
-        $this->userID = 42;
         $data = [
-            'locationId' => 1,
+            'spaceId' => [
+                'id' => 15
+            ],
+            'participantsCurrent' => 1,
             'start' => [
                 '2018-12-12 12:00:00',
                 '2018-12-13 12:00:00',
@@ -166,150 +167,157 @@ class sessionsCest
                 '2018-12-12 13:00:00',
                 '2018-12-13 13:00:00',
                 '2018-12-14 13:00:00'
-            ]
-        ];
-        $I->sendPUT($this->route.'/'.$this->userID.'/trainings', $data);
+            ],
+            'trainingIds' => [
+                1,
+                2,
+                3
+        ]];
+        $I->sendPUT($this->route.'/'.$this->userID[0].'/trainings', $data);
         $I->seeResponseCodeIs(200);
     }
 
-    //-------------------Delete trainings by Session ID----------------------//
+    //-------------- Send Get Listing of Trainings by Session ID   -----------//
+    /**
+     * @param ApiTester $I
+     * @before signInByPassword
+     */
+    public function sendGetListingOfTrainingsBySessionID(ApiTester $I)
+    {
+        $I->sendGet($this->route.'/'.$this->userID[0].'/trainings');
+        $I->seeResponseCodeIs(200);
+    }
 
+    //-------------   Send Delete Session By Id --------------------//
+    /**
+     * @param ApiTester $I
+     * @before signInByPassword
+     */
+    public function sendDeleteSessionById(ApiTester $I)
+    {
+        $I->sendDELETE($this->route.'/'.$this->userID[0]);
+        $I->seeResponseCodeIs(204);
+    }
+
+    //-------------- Send Delete trainings by Session ID  -----------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendDeleteTrainingsBySessionId(ApiTester $I)
     {
-        $this->userID = 42;
-        $I->sendDELETE($this->route.'/'.$this->userID.'/trainings');
+        $I->sendDELETE($this->route.'/'.$this->userID[0].'/trainings');
         $I->seeResponseCodeIs(204);
     }
 
-    //----------------Listing of activities by Session ID---------------------//
-
+    //-------------  Send Get Listing of Activities by Session ID   --------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendGetListingOfActivitiesBySessionID(ApiTester $I)
     {
-        $this->userID = 42;
-        $I->sendGET($this->route.'/'.$this->userID.'/activities');
+        $I->sendGET($this->route.'/'.$this->userID[0].'/activities');
         $I->seeResponseCodeIs(200);
     }
 
-    //----------------Create activities by Session ID----------------------//
-
+    //-------------- Send Post Create activities by Session ID   ------------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendPostCreateActivitiesBySessionID(ApiTester $I)
     {
-        $this->userID = 42;
         $data = [
-            "activities" => [
+            'activities' => [
                 fake::create()->randomNumber(1,true),
                 fake::create()->randomNumber(1,true),
                 fake::create()->randomNumber(1,true)
         ]];
-        $I->sendPOST($this->route.'/'.$this->userID.'/activities', $data);
+        $I->sendPOST($this->route.'/'.$this->userID[0].'/activities', $data);
         $I->seeResponseCodeIs(200);
     }
 
-    //-----------------Update activities by Session ID--------------------//
-
+    //--------------  Send Put Update activities by Session ID  --------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendPutUpdateActivitiesBySessionID(ApiTester $I)
     {
-        $this->userID = 42;
         $data = [
-            "activities" => [
+            'activities' => [
                 fake::create()->randomNumber(1,true),
                 fake::create()->randomNumber(1,true),
                 fake::create()->randomNumber(1,true)
             ]];
-        $I->sendPUT($this->route.'/'.$this->userID.'/activities', $data);
+        $I->sendPUT($this->route.'/'.$this->userID[0].'/activities', $data);
         $I->seeResponseCodeIs(200);
     }
 
-    //------------------Delete activities by Session ID---------------//
-
+    //--------------- Send Delete Activities by Session ID   ---------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendDeleteActivitiesBySessionID(ApiTester $I)
     {
-        $this->userID = 42;
-        $I->sendDELETE($this->route.'/'.$this->userID.'/activities');
+        $I->sendDELETE($this->route.'/'.$this->userID[0].'/activities');
         $I->seeResponseCodeIs(204);
     }
 
-    //-----------------Listing of coaches by Session ID--------------//
-
+    //-------------- Send Get Listing of Coaches by Session ID  --------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendGetListingOfCoachesBySessionID(ApiTester $I)
     {
-        $this->userID = 42;
-        $I->sendGET($this->route.'/'.$this->userID.'/coaches');
+        $I->sendGET($this->route.'/'.$this->userID[0].'/coaches');
         $I->seeResponseCodeIs(200);
     }
 
-    //-----------------Create coaches by Session ID--------------//
-
+    //--------------- Send Post Create Coach by Session ID  --------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
-    public function sendPostCreateCoachesBySessionID(ApiTester $I)
+    public function sendPostCreateCoachBySessionID(ApiTester $I)
     {
-        $this->userID = 42;
         $data = [
-            "coaches" => [
+            "coach" => [
+                fake::create()->randomNumber(1,true)],
+            'userId' => 1
+        ];
+        $I->sendPOST($this->route.'/'.$this->userID[0].'/coaches', $data);
+        $I->seeResponseCodeIs(200);
+    }
+
+    //-------------   Send Put Update Coach by Session ID  ---------------//
+    /**
+     * @param ApiTester $I
+     * @before signInByPassword
+     */
+    public function sendPutUpdateCoachBySessionID (ApiTester $I)
+    {
+        $data = [
+            "coach" => [
                 fake::create()->randomNumber(1,true),
                 fake::create()->randomNumber(1,true),
                 fake::create()->randomNumber(1,true)
             ]];
-        $I->sendPOST($this->route.'/'.$this->userID.'/coaches', $data);
+        $I->sendPUT($this->route.'/'.$this->userID[0].'/coaches', $data);
         $I->seeResponseCodeIs(200);
     }
 
-    //----------------Update coaches by Session ID---------------//
-
+    //--------------  Send Delete Coaches by Session ID   --------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
-    public function sendPutUpdateCoachesBySessionID (ApiTester $I)
+    public function sendDeleteCoachBySessionID(ApiTester $I)
     {
-        $this->userID = 42;
-        $data = [
-            "coaches" => [
-                fake::create()->randomNumber(1,true),
-                fake::create()->randomNumber(1,true),
-                fake::create()->randomNumber(1,true)
-            ]];
-        $I->sendPUT($this->route.'/'.$this->userID.'/coaches', $data);
-        $I->seeResponseCodeIs(200);
-    }
-
-    //-----------------Delete coaches by Session ID--------------//
-
-    /**
-     * @param ApiTester $I
-     * @before signInByPassword
-     */
-    public function sendDeleteCoachesBySessionID(ApiTester $I)
-    {
-        $this->userID = 42;
-        $I->sendDELETE($this->route.'/'.$this->userID.'/coaches');
+        $I->sendDELETE($this->route.'/'.$this->userID[0].'/coaches');
         $I->seeResponseCodeIs(204);
     }
 

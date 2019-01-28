652a6653ca0463c5ec3ebe145bb403fe68b8e82e
Yurii17 <37590571+Yurii17@users.noreply.github.com>
2019-01-28 18:23:09 +0200

    release v1

--- tests/api/equipmentsCest.php
+++ tests/api/equipmentsCest.php
@@ -20,16 +20,43 @@ class equipmentsCest
         $I->loginAs("yurii.lobas+e769b642eaa052d122fe4e6359f83f79@gmail.com", "8_yry7p>+-[fWg^.");
     }
 
-    public function _before(ApiTester $I)
-    {
-    }
-
     //------------- Send Get Listing of equipments --------------------//
 
     public function sendGetListingOfEquipments(ApiTester $I)
     {
         $I->sendGET($this->route);
         $I->seeResponseCodeIs(200);
+    }
+
+    //-------------- Send Post Create new equipment -------------//
+    /**
+     * @param ApiTester $I
+     * @throws Exception
+     * @before signInByPassword
+     */
+    public function sendPostCreateNewEquipment(ApiTester $I)
+    {
+        $data = [
+            'id' => '1',
+            'code' => 'TV',
+            'name' => fake::create()->text(20),
+            'createdAt' => '2019',
+            'modifiedAt' => '2019',
+            'expireAt' => '01',
+            'status' => 'active'
+        ];
+        $I->saveEquipments([
+            $data['id'], ' ',
+            $data['code'], ' ',
+            $data['name'], ' ',
+            $data['createdAt'], ' ',
+            $data['modifiedAt'], ' ',
+            $data['expireAt'], ' ',
+            $data['status'], ' '
+        ], 'equipments.txt');
+        $I->sendPOST($this->route, $data);
+        $this->userID = $I->grabDataFromResponseByJsonPath('$.id');
+        $I->seeResponseCodeIs(201);
     }
 
     //------------- Send Get Show equipment BY ID --------------------//
@@ -39,24 +66,8 @@ class equipmentsCest
      */
     public function sendGetShowEquipmentByID(ApiTester $I)
     {
-        $this->userID = 1;
-        $I->sendGET($this->route.'/'.$this->userID);
+        $I->sendGET($this->route.'/'.$this->userID[0]);
         $I->seeResponseCodeIs(200);
-    }
-
-    //-------------- Send Post Create new equipment -------------//
-    /**
-     * @param ApiTester $I
-     * @before signInByPassword
-     */
-    public function sendPostCreateNewEquipment(ApiTester $I)
-    {
-        $data = [
-            'code' => 'TV',
-            'name' => fake::create()->text(20)
-        ];
-        $I->sendPOST($this->route, $data);
-        $I->seeResponseCodeIs(201);
     }
 
     //-------------- Send Put Modify equipment By ID -------------//
@@ -66,34 +77,82 @@ class equipmentsCest
      */
     public function sendPutModifyEquipmentByID(ApiTester $I)
     {
-        $this->userID = 1;
         $data = [
-            'code' => 'TV',
-            'name' => fake::create()->text(20)
+            'id' => '1',
+            'code' => fake::create()->text(10),
+            'name' => fake::create()->text(20),
+            'createdAt' => '2019',
+            'modifiedAt' => '2019',
+            'expireAt' => '01',
+            'status' => 'active'
         ];
-        $I->sendGET($this->route.'/'.$this->userID, $data);
+        $I->sendPUT($this->route.'/'.$this->userID[0], $data);
         $I->seeResponseCodeIs(200);
     }
 
-    //-------------- Send Delete equipment -------------//
+    //-------------- Send Delete equipment By ID -------------//
     /**
      * @param ApiTester $I
      * @before signInByPassword
      */
     public function sendDeleteEquipmentByID(ApiTester $I)
     {
-        $this->userID = 2;
-        $I->sendDELETE($this->route.'/'.$this->userID);
+        $I->sendDELETE($this->route.'/'.$this->userID[0]);
         $I->seeResponseCodeIs(204);
     }
 
+    //-------------- Send Post Create new equipment Empty Valid -------------//
+    public function sendPostCreateNewEquipmentEmptyValid(ApiTester $I)
+    {
+        $data = [
+        ];
+        $I->sendPOST($this->route, $data);
+        $I->seeResponseCodeIs(201);
+    }
 
+    //-------------- Send Post Create new equipment Name Error -------------//
+    public function sendPostCreateNewEquipmentNameError(ApiTester $I)
+    {
+        $data = [
+            'code' => 'TV',
+            'name' => fake::create()->text(100),
+            'createdAt' => '2019',
+            'modifiedAt' => '2019',
+            'expireAt' => '01',
+            'status' => 'active'
+        ];
+        $I->sendPOST($this->route, $data);
+        $I->seeErrorMessage([
+            'field' => 'name',
+            'message' => 'Name should contain at most 32 characters.'
+        ]);
+    }
 
+    //-------------- Send Post Create new equipment Code Error -------------//
+    public function sendPostCreateNewEquipmentCodeError(ApiTester $I)
+    {
+        $data = [
+            'code' => fake::create()->text(50)
+        ];
+        $I->sendPOST($this->route, $data);
+        $I->seeErrorMessage([
+            'field' => 'code',
+            'message' => 'Code should contain at most 10 characters.'
+        ]);
+    }
 
-
-
-
-
+    //-------------- Send Post Create new equipment Status Error -------------//
+    public function sendPostCreateNewEquipmentStatusError(ApiTester $I)
+    {
+        $data = [
+            'status' => fake::create()->text(50)
+        ];
+        $I->sendPOST($this->route, $data);
+        $I->seeErrorMessage([
+            'field' => 'status',
+            'message' => 'Status should contain at most 8 characters.'
+        ]);
+    }
 
 
 

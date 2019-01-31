11cc3f92b6efb9dcf4d3d04c18abfe2e2e9a00cb
Yurii17 <37590571+Yurii17@users.noreply.github.com>
2019-01-31 17:34:40 +0200

    release v1

--- tests/_support/Helper/Api.php
+++ tests/_support/Helper/Api.php
@@ -6,5 +6,26 @@ namespace Helper;
 
 class Api extends \Codeception\Module
 {
+    public function _afterSuite()
+    {
+        $path = codecept_data_dir() . 'image';
+        if (!is_dir($path)) {
+            echo "Cannot find $path\n";
+            return false;
+        } else {
+            $rep = strlen($path . '/');
+            $files = glob($path . '/' . "*.png");
+            $fileData = [];
+            foreach ($files as $key) {
+                $file = substr($key, $rep);
+                array_push($fileData, $file);
+            }
+            if (is_array($fileData)) {
+                foreach ($fileData as $key) {
+                    unlink(codecept_data_dir() . 'image/' . $key);
+                }
+            }
+        }
+    }
 
 }
